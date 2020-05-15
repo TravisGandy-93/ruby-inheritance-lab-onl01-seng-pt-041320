ff2cd642aec3f38e96848d64f60f824095a0d1df
Travis Gandy <travis_gandy@yahoo.com>
2020-05-15 17:00:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,10 @@
 class User 
   attr_writer :first_name, :last_name
+  attr_reader :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+      @first_name = first_name
+      @last_name = last_name
+  end 
   
 end 
